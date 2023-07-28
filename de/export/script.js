9b72c56f3a0e6c52bc6a1244b43949469b00d777
Tim Alber <t.midaidu@gmail.com>
2023-07-28 13:22:53 +0200

    added 3

--- de/export/script.js
+++ de/export/script.js
@@ -6,6 +6,8 @@ function loadFromLocalStorage() {
     const data = JSON.parse(localStorage.getItem("maastricht-interview-data"));
     setMetaData(data.meta_data);
     setSectionOne(data.section_1);
+    setSectionTwo(data.section_2);
+    setSectionThree(data.section_3);
 }
 
 function setMetaData(data) {
@@ -37,6 +39,144 @@ function setSectionOne(data) {
     document.getElementById('q-1.5.3').innerHTML = formatRadioButtons(data.q_1_5_sub.q_1_5_3)
     document.getElementById('q-1.5.4').innerHTML = formatRadioButtons(data.q_1_5_sub.q_1_5_4)
     document.getElementById('q-1.5.5').innerHTML = formatRadioButtons(data.q_1_5_sub.q_1_5_5)
+}
+
+function setSectionTwo(data){
+    document.getElementById('q-2.1').innerHTML = data.q_2_1
+    drawTable(data.q_2_2)
+    document.getElementById('q-2.3').innerHTML = data.q_2_3
+}
+
+function setSectionThree(data){
+    document.getElementById('q-3.1').innerHTML = data.q_3_1
+    document.getElementById('q-3.2').innerHTML = data.q_3_2
+
+    document.getElementById('q-3.3.1.yesNo').innerHTML = formatRadioButtons(data.q_3_3.q_3_3_1.yesNo)
+    document.getElementById('q-3.3.1.age').innerHTML = data.q_3_3.q_3_3_1.age
+    document.getElementById('q-3.3.1.voices').innerHTML = data.q_3_3.q_3_3_1.voices
+
+    document.getElementById('q-3.3.2.yesNo').innerHTML = formatRadioButtons(data.q_3_3.q_3_3_2.yesNo)
+    document.getElementById('q-3.3.2.age').innerHTML = data.q_3_3.q_3_3_2.age
+    document.getElementById('q-3.3.2.voices').innerHTML = data.q_3_3.q_3_3_2.voices
+
+    document.getElementById('q-3.3.3.yesNo').innerHTML = formatRadioButtons(data.q_3_3.q_3_3_3.yesNo)
+    document.getElementById('q-3.3.3.age').innerHTML = data.q_3_3.q_3_3_3.age
+    document.getElementById('q-3.3.3.voices').innerHTML = data.q_3_3.q_3_3_3.voices
+
+    document.getElementById('q-3.3.4.yesNo').innerHTML = formatRadioButtons(data.q_3_3.q_3_3_4.yesNo)
+    document.getElementById('q-3.3.4.age').innerHTML = data.q_3_3.q_3_3_4.age
+    document.getElementById('q-3.3.4.voices').innerHTML = data.q_3_3.q_3_3_4.voices
+
+    document.getElementById('q-3.3.5.yesNo').innerHTML = formatRadioButtons(data.q_3_3.q_3_3_5.yesNo)
+    document.getElementById('q-3.3.5.age').innerHTML = data.q_3_3.q_3_3_5.age
+    document.getElementById('q-3.3.5.voices').innerHTML = data.q_3_3.q_3_3_5.voices
+
+    document.getElementById('q-3.3.6.yesNo').innerHTML = formatRadioButtons(data.q_3_3.q_3_3_6.yesNo)
+    document.getElementById('q-3.3.6.age').innerHTML = data.q_3_3.q_3_3_6.age
+    document.getElementById('q-3.3.6.voices').innerHTML = data.q_3_3.q_3_3_6.voices
+
+    document.getElementById('q-3.3.7.yesNo').innerHTML = formatRadioButtons(data.q_3_3.q_3_3_7.yesNo)
+    document.getElementById('q-3.3.7.age').innerHTML = data.q_3_3.q_3_3_7.age
+    document.getElementById('q-3.3.7.voices').innerHTML = data.q_3_3.q_3_3_7.voices
+
+    document.getElementById('q-3.3.8.yesNo').innerHTML = formatRadioButtons(data.q_3_3.q_3_3_8.yesNo)
+    document.getElementById('q-3.3.8.age').innerHTML = data.q_3_3.q_3_3_8.age
+    document.getElementById('q-3.3.8.voices').innerHTML = data.q_3_3.q_3_3_8.voices
+
+    document.getElementById('q-3.3.9.yesNo').innerHTML = formatRadioButtons(data.q_3_3.q_3_3_9.yesNo)
+    document.getElementById('q-3.3.9.age').innerHTML = data.q_3_3.q_3_3_9.age
+    document.getElementById('q-3.3.9.voices').innerHTML = data.q_3_3.q_3_3_9.voices
+
+    document.getElementById('q-3.3.10.yesNo').innerHTML = formatRadioButtons(data.q_3_3.q_3_3_10.yesNo)
+    document.getElementById('q-3.3.10.age').innerHTML = data.q_3_3.q_3_3_10.age
+    document.getElementById('q-3.3.10.voices').innerHTML = data.q_3_3.q_3_3_10.voices
+
+    document.getElementById('q-3.3.11.yesNo').innerHTML = formatRadioButtons(data.q_3_3.q_3_3_11.yesNo)
+    document.getElementById('q-3.3.11.age').innerHTML = data.q_3_3.q_3_3_11.age
+    document.getElementById('q-3.3.11.voices').innerHTML = data.q_3_3.q_3_3_11.voices
+
+    document.getElementById('q-3.3.12.yesNo').innerHTML = formatRadioButtons(data.q_3_3.q_3_3_12.yesNo)
+    document.getElementById('q-3.3.12.age').innerHTML = data.q_3_3.q_3_3_12.age
+    document.getElementById('q-3.3.12.voices').innerHTML = data.q_3_3.q_3_3_12.voices
+
+    document.getElementById('q-3.3.13.yesNo').innerHTML = formatRadioButtons(data.q_3_3.q_3_3_13.yesNo)
+    document.getElementById('q-3.3.13.age').innerHTML = data.q_3_3.q_3_3_13.age
+    document.getElementById('q-3.3.13.voices').innerHTML = data.q_3_3.q_3_3_13.voices
+
+    document.getElementById('q-3.3.14.yesNo').innerHTML = formatRadioButtons(data.q_3_3.q_3_3_14.yesNo)
+    document.getElementById('q-3.3.14.age').innerHTML = data.q_3_3.q_3_3_14.age
+    document.getElementById('q-3.3.14.voices').innerHTML = data.q_3_3.q_3_3_14.voices
+
+    document.getElementById('q-3.3.15.yesNo').innerHTML = formatRadioButtons(data.q_3_3.q_3_3_15.yesNo)
+    document.getElementById('q-3.3.15.age').innerHTML = data.q_3_3.q_3_3_15.age
+    document.getElementById('q-3.3.15.voices').innerHTML = data.q_3_3.q_3_3_15.voices
+
+    document.getElementById('q-3.3.16.yesNo').innerHTML = formatRadioButtons(data.q_3_3.q_3_3_16.yesNo)
+    document.getElementById('q-3.3.16.age').innerHTML = data.q_3_3.q_3_3_16.age
+    document.getElementById('q-3.3.16.voices').innerHTML = data.q_3_3.q_3_3_16.voices
+
+    document.getElementById('q-3.3.17.yesNo').innerHTML = formatRadioButtons(data.q_3_3.q_3_3_17.yesNo)
+    document.getElementById('q-3.3.17.age').innerHTML = data.q_3_3.q_3_3_17.age
+    document.getElementById('q-3.3.17.voices').innerHTML = data.q_3_3.q_3_3_17.voices
+
+    document.getElementById('q-3.3.18.yesNo').innerHTML = formatRadioButtons(data.q_3_3.q_3_3_18.yesNo)
+    document.getElementById('q-3.3.18.age').innerHTML = data.q_3_3.q_3_3_18.age
+    document.getElementById('q-3.3.18.voices').innerHTML = data.q_3_3.q_3_3_18.voices
+
+    document.getElementById('q-3.3.19.yesNo').innerHTML = formatRadioButtons(data.q_3_3.q_3_3_19.yesNo)
+    document.getElementById('q-3.3.19.age').innerHTML = data.q_3_3.q_3_3_19.age
+    document.getElementById('q-3.3.19.voices').innerHTML = data.q_3_3.q_3_3_19.voices
+
+    document.getElementById('q-3.3.19.yesNo').innerHTML = formatRadioButtons(data.q_3_3.q_3_3_19.yesNo)
+    document.getElementById('q-3.3.19.age').innerHTML = data.q_3_3.q_3_3_19.age
+    document.getElementById('q-3.3.19.voices').innerHTML = data.q_3_3.q_3_3_19.voices
+
+    document.getElementById('q-3.3.20.yesNo').innerHTML = formatRadioButtons(data.q_3_3.q_3_3_20.yesNo)
+    document.getElementById('q-3.3.20.age').innerHTML = data.q_3_3.q_3_3_20.age
+    document.getElementById('q-3.3.20.voices').innerHTML = data.q_3_3.q_3_3_20.voices
+
+    document.getElementById('q-3.3.21.yesNo').innerHTML = formatRadioButtons(data.q_3_3.q_3_3_21.yesNo)
+    document.getElementById('q-3.3.21.age').innerHTML = data.q_3_3.q_3_3_21.age
+    document.getElementById('q-3.3.21.voices').innerHTML = data.q_3_3.q_3_3_21.voices
+
+    document.getElementById('q-3.3.22.yesNo').innerHTML = formatRadioButtons(data.q_3_3.q_3_3_22.yesNo)
+    document.getElementById('q-3.3.22.age').innerHTML = data.q_3_3.q_3_3_22.age
+    document.getElementById('q-3.3.22.voices').innerHTML = data.q_3_3.q_3_3_22.voices
+
+    document.getElementById('q-3.3.23.yesNo').innerHTML = formatRadioButtons(data.q_3_3.q_3_3_23.yesNo)
+    document.getElementById('q-3.3.23.age').innerHTML = data.q_3_3.q_3_3_23.age
+    document.getElementById('q-3.3.23.voices').innerHTML = data.q_3_3.q_3_3_23.voices
+
+    document.getElementById('q-3.3.24.yesNo').innerHTML = formatRadioButtons(data.q_3_3.q_3_3_24.yesNo)
+    document.getElementById('q-3.3.24.age').innerHTML = data.q_3_3.q_3_3_24.age
+    document.getElementById('q-3.3.24.voices').innerHTML = data.q_3_3.q_3_3_24.voices
+
+    document.getElementById('q-3.3.25.text').innerHTML = data.q_3_3.q_3_3_25.text
+    document.getElementById('q-3.3.25.yesNo').innerHTML = formatRadioButtons(data.q_3_3.q_3_3_25.yesNo)
+    document.getElementById('q-3.3.25.age').innerHTML = data.q_3_3.q_3_3_25.age
+    document.getElementById('q-3.3.25.voices').innerHTML = data.q_3_3.q_3_3_25.voices
+
+    document.getElementById('q-3.3.26.text').innerHTML = data.q_3_3.q_3_3_26.text
+    document.getElementById('q-3.3.26.yesNo').innerHTML = formatRadioButtons(data.q_3_3.q_3_3_26.yesNo)
+    document.getElementById('q-3.3.26.age').innerHTML = data.q_3_3.q_3_3_26.age
+    document.getElementById('q-3.3.26.voices').innerHTML = data.q_3_3.q_3_3_26.voices
+
+    document.getElementById('q-3.3.27.text').innerHTML = data.q_3_3.q_3_3_27.text
+    document.getElementById('q-3.3.27.yesNo').innerHTML = formatRadioButtons(data.q_3_3.q_3_3_27.yesNo)
+    document.getElementById('q-3.3.27.age').innerHTML = data.q_3_3.q_3_3_27.age
+    document.getElementById('q-3.3.27.voices').innerHTML = data.q_3_3.q_3_3_27.voices
+}
+
+function drawTable(data) {
+    var table = document.getElementById("q-2.2");
+    for (let row of data) {
+        var newRow = table.insertRow(table.rows.length);
+        for (let i = 0; i < row.length; i++) {
+            var currentCell = newRow.insertCell(i);
+            currentCell.innerHTML = row[i]
+            currentCell.style.textAlign = "center"
+        }
+    }
 }
 
 function reformatDate(stringDate) {
